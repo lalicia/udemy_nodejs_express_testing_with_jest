002f1289f0d44236f66c705f4880714c0c4ba3a4
alicialowe <hohpascal@outlook.com>
2023-06-29 08:36:27 +0100

    feat: integration tests added for getTodoByID

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createTodo, getTodos } from "../controllers/todoController.js";
+import {
+  createTodo,
+  getTodos,
+  getTodoByID,
+} from "../controllers/todoController.js";
 
 export const router = express.Router();
 
 router.post("/", createTodo);
 
 router.get("/", getTodos);
+
+router.get("/:todoId", getTodoByID);
--- test/integration/todoControllerInt.test.js
+++ test/integration/todoControllerInt.test.js
@@ -17,6 +17,9 @@ const allTodos = [
   },
 ];
 
+//got GET by ID we're dealing with a live db, so how to we get a specific one? Here we declare a variable and then in our GET test we can set the first value as the variable
+let firstTodo;
+
 const endpointUrl = "/todos/";
 
 describe(endpointUrl, () => {
@@ -30,6 +33,26 @@ describe(endpointUrl, () => {
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].title).toBeDefined();
     expect(response.body[0].done).toBeDefined();
+    // setting variable for GET by ID
+    firstTodo = response.body[0];
+  });
+
+  test("GET by ID" + endpointUrl, async () => {
+    // uses firstTodo which we set in the GET request
+    const response = await request(app).get(endpointUrl + firstTodo._id);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe(firstTodo.title);
+    expect(response.body.done).toBe(firstTodo.done);
+  });
+
+  test("GET todo by ID doesn't exist" + endpointUrl, async () => {
+    // needs to be an ID that doesn't exist but in correct format, otherwise mongoose will identify it's not a valid ID and will throw a 500 error instead of 404
+    const response = await request(app).get(
+      endpointUrl + "6435a3f4510949223f5a0805"
+    );
+
+    expect(response.statusCode).toBe(404);
   });
 
   it(
